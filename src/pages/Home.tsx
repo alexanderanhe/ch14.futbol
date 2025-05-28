d99f6d54b3c40a58b51fa5f0bf31fb5c665779a9
Alexander Angulo <alex.anguloh@gmail.com>
2025-05-27 18:01:53 -0600

    wip()

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import Feed from '../components/feed';
+import Loader from '../components/loader';
 
 export default function HomePage() {
   return (
-    <Suspense fallback={<p>Cargando</p>}>
+    <Suspense fallback={<Loader />}>
       <Feed />
     </Suspense>
   )
